0b0a4e6fbb21b5730d95ba86200c1f1d6e1cf742
Sandeep <117218272+Sandeepnine09@users.noreply.github.com>
2023-06-15 21:40:58 +0530

    Update ArrayListAccount.java

--- ArrayListAccount.java
+++ ArrayListAccount.java
@@ -84,3 +84,99 @@ public Stream Execute(JObject graphQLResult, bool ignorePropertyPath)
             }
             return null;
         }
+
+How can I make multiple sheets inside Excel along with sheet1 with same data of sheet1 and along with that how WriteHeader function should change
+
+public Stream Execute(JObject graphQLResult, bool ignorePropertyPath)
+{
+    var list = graphQLResult.SelectToken("$.search.list") ?? graphQLResult.SelectToken("$..search.list")
+        ?? graphQLResult.SelectToken("$..search");
+    if (list != null && list is JArray)
+    {
+        var ms = new MemoryStream();
+        using (var excelStream = new MemoryStream())
+        {
+            var workbook = new XSSFWorkbook();
+
+            var dictList = (list as JArray).ToFlatDictionaries();
+
+            // Create the initial sheet "Sheet1" and copy data
+            var sheet1 = (ISheet)workbook.CreateSheet("Sheet1");
+            var header = WriteHeader(dictList, sheet1, ignorePropertyPath);
+            var rowIndex = WriteRows(header, dictList, sheet1, 0, ignorePropertyPath);
+
+            // Create additional sheets and copy data from "Sheet1"
+            for (int i = 1; i < 5; i++) // Example: Creating 4 additional sheets
+            {
+                var sheetName = "Sheet" + (i + 1);
+                var newSheet = (ISheet)workbook.CloneSheet(0);
+                workbook.SetSheetName(workbook.GetSheetIndex(newSheet), sheetName);
+                WriteRows(header, dictList, newSheet, 0, ignorePropertyPath);
+            }
+
+            workbook.Write(excelStream);
+            byte[] excelStreamContent = excelStream.ToArray();
+            ms.Write(excelStreamContent, 0, excelStreamContent.Length);
+        }
+        return ms;
+    }
+    return null;
+}
+
+private Dictionary<string, int> WriteHeader(List<Dictionary<string, object>> dictList, ISheet sheet, bool ignorePropertyPath)
+{
+    var orderedHeaders = dictList.SelectMany(lst => lst.Select((row, index) => new { text = row.Key, index }));
+    var allHeaders = orderedHeaders
+        .GroupBy(gp => new { gp.text })
+        .OrderBy(x => x.Max(y => y.index))
+        .Select(x => x.Key.text)
+        .Distinct();
+
+    var header = new Dictionary<string, int>();
+    var headerRow = sheet.CreateRow(0);
+    int columnIndex = 0;
+
+    foreach (var headerText in allHeaders)
+    {
+        var cell = headerRow.CreateCell(columnIndex);
+        cell.SetCellValue(headerText);
+        header[headerText] = columnIndex;
+        columnIndex++;
+    }
+
+    return header;
+}
+
+private int WriteRows(Dictionary<string, int> header, List<Dictionary<string, object>> dictList, ISheet sheet, int startIndex, bool ignorePropertyPath)
+{
+    var rowIndex = startIndex;
+
+    foreach (var record in dictList)
+    {
+        var dataRow = sheet.CreateRow(rowIndex + 1); // Start from row index 1, as row 0 is for headers
+
+        foreach (var keyValuePair in record)
+        {
+            var columnIndex = header[keyValuePair.Key];
+            var cell = dataRow.CreateCell(columnIndex);
+            var cellValue = keyValuePair.Value;
+
+            if (cellValue != null && cellValue.GetType() == typeof(string))
+            {
+                cellValue = ((string)cellValue).Replace("\r", "");
+            }
+
+            cell.SetCellValue(cellValue.ToString());
+        }
+
+        rowIndex++;
+    }
+
+    return rowIndex;
+}
+
+In this modified code, I added a loop to create additional sheets and copied the data from "Sheet1" to each new sheet. I used the CloneSheet method to clone the "Sheet1" and set a new sheet name using SetSheetName. Then, I called the WriteRows function to copy the data from dictList to the new sheet.
+
+The WriteHeader function remains the same as before, as it creates the headers for the first sheet. However, I changed the WriteRow function to WriteRows to handle writing multiple rows.
+
+Please note that in this example, I created four additional sheets (Sheet2, Sheet3, Sheet4, and Sheet5). You can adjust the loop condition (i < 5) and sheet names as per your requirements.
