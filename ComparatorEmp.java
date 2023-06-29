62cdd338da91bbda444847e358bb5f4f7811dbd6
Sandeep <117218272+Sandeepnine09@users.noreply.github.com>
2023-06-29 12:38:02 +0530

    Update ComparatorEmp.java

--- ComparatorEmp.java
+++ ComparatorEmp.java
@@ -10,3 +10,8 @@ public class ComparatorEmp implements Comparator<employee>{
 		return (int)(e1.getEmpDeptNo() - e2.getEmpDeptNo());
 	}
 }
+
+As a worker at VWITS, I continually try to infuse the office with energy by incorporating the value of technology leadership into my routine workday. Technology, in my opinion, is a key factor in fostering innovation, efficiency, and teamwork inside an organisation.
+
+	To begin with, I am always looking for ways to incorporate cutting-edge technologies that can boost productivity and streamline our processes. I am constantly looking for ways to use technology for the benefit of the entire team, whether it is through implementing cloud-based applications for seamless collaboration or using automation to remove manual duties.
+	To encourage efficient teamwork and information exchange, I advocate the use of digital communication channels and project management systems. We are able to work more productively and swiftly accomplish our objectives by utilising technology to streamline communication and project procedures.
